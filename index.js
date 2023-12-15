3f222ada8b55735b542e14d332e017976cccb445
Clint <incineratednewt@gmail.com>
2023-12-15 13:32:02 +1300

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const Note = require("./models/note");
 
 const app = express();
 
@@ -43,17 +45,14 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then(response.json(notes));
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else response.status(404).end();
+  });
 });
-
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
@@ -63,16 +62,19 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
-  if (!body.content) {
+
+  if (body.content === undefined) {
     return response.status(400).json({ error: "content missing" });
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
-  notes = notes.concat(note);
-  response.json(notes);
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
